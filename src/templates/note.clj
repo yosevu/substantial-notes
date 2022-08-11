8caa453aa8e0cf67d902d1a5633ec8c68686dae9
Yosevu Kilonzo <yosevuk@gmail.com>
2022-08-12 07:38:36 +0900

    refactor: use substantial template

--- src/templates/note.clj
+++
@@ -1,9 +0,0 @@
-(ns templates.note
-  (:require [templates.partials :refer [create-page create-head create-body]]))
-
-(defn create-note-page
-  "Create a page for a note."
-  [config {:keys [metadata html backlinks]}]
-  (create-page
-   (create-head config)
-   (create-body config [html backlinks])))
